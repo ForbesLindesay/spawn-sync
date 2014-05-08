333c1bc759dcabe11fdbb821c27abf8d27c944f0
Dhruv Matani <dhruvbird@fb.com>
2014-05-08 12:14:14 -0700

    Use temporary directory to store stdout/stderr/stdin/finished flag files instead of the same directory, which prevents [1] use of a read-only file system to store the module, and [2] multiple simultaneous processes using spawn-sync at the same time

--- read.js
+++ read.js
@@ -1,7 +1,13 @@
 'use strict';
 
-// This program reads the input to you can pipe some input text to a command
+// This program reads the input so you can pipe some input text to a command
 
 var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var assert = require('assert').ok;
 
-fs.createReadStream(__dirname + '/temp/input').pipe(process.stdout);
+var inputFilePath = process.argv[2];
+assert(inputFilePath && inputFilePath.length > 0);
+
+fs.createReadStream(inputFilePath).pipe(process.stdout);
