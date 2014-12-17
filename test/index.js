6b7fc63cc7e7874f5df1ea8915e2b328ff14fb2c
Forbes Lindesay <forbes@lindesay.co.uk>
2014-12-17 04:17:55 +0000

    Silence warnings in test cases
    Hopefully this will make AppVeyor happy

--- test/index.js
+++ test/index.js
@@ -33,7 +33,7 @@ function getSpawn(require) {
     exports: exports,
     require: require,
     __dirname: path.dirname(spawnFile),
-    console: console,
+    console: {warn: function () {}},
     process: process
   };
   vm.runInNewContext(spawnSource, context, spawnFile);
