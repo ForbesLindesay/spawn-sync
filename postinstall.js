389db8d49a6de3db5c67481530f9db2d885e5f99
Forbes Lindesay <forbes@lindesay.co.uk>
2015-02-11 13:29:38 +0000

    Have another go at fixing postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -21,7 +21,7 @@ if (cp.spawnSync || __dirname.indexOf('node_modules') === -1) {
     REQUIRES_UPDATE = true;
   }
 }
-if (REQUIRES_UPDATE && __dirname.indexOf('node_modules') === -1) {
+if (REQUIRES_UPDATE && __dirname.indexOf('node_modules') !== -1) {
   fs.writeFileSync(__dirname + '/package.json', JSON.stringify(pkg, null, '  '));
   cp.exec('npm prune --production && npm install --production', {
     cwd: __dirname
